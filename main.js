d096da1bd31ba02c7263679a09bab3aceaa9cb83
kay-natsumi <kyonehara.work@gmail.com>
2022-09-27 13:24:24 +0900

    Update 2022

--- main.js
+++ main.js
@@ -1,63 +1,18 @@
- ///var theButton = document.getElementById("portfolio");
-
- ///theButton.onclick = function () {
-  ///var password = prompt("Please enter the password.");
-  ///   if (password==="pugs!2020"){
-  ///       window.open("files/Portfolio2020.pdf","_blank");
-  ///   } else{
- ///     password = prompt("oops! wrong password");
-  ///   }
- ///}
-
-
-$(function() {
-  $(".translate").click(function() {
-   $(".translate").removeClass("on");
-  $(this).addClass("on");
-  });
-});
-
- /// $(function() {
-///    $("#language-toggle").click(function(){  
-///        $(".text-on").addClass("text-on").removeClass("text-off");
-///        $(".text-off").addClass("text-off").removeClass("text-on");
-///    });
-///  });
-
- ///$(window).load(function () {
-///     $("#top").slideDown();
-/// });
-
-var slideUp = {
-    distance: '150%',
-    origin: 'bottom',
-    opacity: null
-};
-
-ScrollReveal().reveal('.intro-about', slideUp);
-ScrollReveal().reveal('.forfun',slideUp);
-ScrollReveal().reveal('.contact',slideUp);
-
- //   $(window).load(function () {
-//        $("#top").show("slide", {
-//            direction: "right"
-//        }, 300);
-//    });
-$(document).ready(function(){
-        $('section').css('width',$(window).width());
-        $(window).resize(function(){
-            $('section').css('width',$(window).width());
-            /* and in your CSS: section { overflow: hidden; } */
-        });
+$(function () {
+  $(".btn")
+    .on("mouseenter", function (e) {
+      var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+      $(this).find("span").css({ top: relY, left: relX });
+    })
+    .on("mouseout", function (e) {
+      var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+      $(this).find("span").css({ top: relY, left: relX });
     });
-$(function() {
-  $('.japanese').hide();
-  $('#jp').click(function() {
-    $('.english').hide();
-    $('.japanese').show();
-  });
-  $('#en').click(function() {
-    $('.english').show();
-    $('.japanese').hide();
+  $("[href=#]").click(function () {
+    return false;
   });
 });
